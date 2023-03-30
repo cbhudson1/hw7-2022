5c49cc691b08a62a4680c4ef205a2761b74dc489
cbhudson1 <78377297+cbhudson1@users.noreply.github.com>
2023-03-30 00:23:26 -0400

    Initial declaration of variables

---
+++ js/video.js
@@ -0,0 +1,17 @@
+//declaring variables before using them//
+
+var video;
+var play = document.querySelector("#play");
+var pause = document.querySelector("#pause");
+var faster = document.querySelector("#faster");
+var slower = document.querySelector("#")
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+

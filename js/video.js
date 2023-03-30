473ae1cb69c986f02ac0e22aa26e765a317a361f
cbhudson1 <78377297+cbhudson1@users.noreply.github.com>
2023-03-30 00:36:56 -0400

    autoplay and loop add-ons

--- js/video.js
+++ js/video.js
@@ -8,10 +8,30 @@ var pause = document.querySelector("#pause");
 var faster = document.querySelector("#faster");
 var slower = document.querySelector("#slower");
 
+var slider = document.querySelector("#slider");
+var volume = document.querySelector("#volume");
+
+var mute = document.querySelector("#mute");
+var skip = document.querySelector("#skip");
+
+var orig = document.querySelector("#orig");
+var vintage = document.querySelector("#vintage");
+
+//initialize declared vars//
+
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+	console.log("Good job opening the window");
+	video = document.querySelector('#player1');
+	
+	video.autoplay = false;
+	console.log("Video autoplay is set to " + video.autoplay);
+
+	video.loop = false;
+	console.log("Video loop is set to " + video.loop);
 
 });
+
+
 
 // document.querySelector("#play").addEventListener("click", function() {
 // 	console.log("Play Video");
